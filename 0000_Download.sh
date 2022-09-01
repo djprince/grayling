9ef9208eeaa14ffeb07c4aba68fadfd5d24fa49c
Dan Prince <djprince@ucdavis.edu>
2022-08-31 17:47:06 -0700

    certificate check on download

--- 0000_Download.sh
+++ 0000_Download.sh
@@ -4,10 +4,10 @@ mkdir slurm_outs/
 mkdir 1001_sequence/
 mkdir 1001_sequence/plates/
 cd 1001_sequence/plates/
-wget http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TACTATAT_R1.fastq.gz
-wget http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TACTATAT_R2.fastq.gz
-wget http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TTAACTAT_R1.fastq.gz
-wget http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TTAACTAT_R2.fastq.gz
+wget --no-check-certificate http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TACTATAT_R1.fastq.gz
+wget --no-check-certificate http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TACTATAT_R2.fastq.gz
+wget --no-check-certificate http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TTAACTAT_R1.fastq.gz
+wget --no-check-certificate http://agri.cse.ucdavis.edu/~djprince/GRAYLING/TTAACTAT_R2.fastq.gz
 gunzip TACTATAT_R1.fastq.gz
 gunzip TACTATAT_R2.fastq.gz
 gunzip TTAACTAT_R1.fastq.gz
